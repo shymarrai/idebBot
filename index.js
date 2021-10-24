e61da5d6a827bc93cf4203ba223d22ffb8d2e154
Bruno Gomes <icestonebruno@gmail.com>
2021-10-24 12:35:52 -0300

    a

--- index.js
+++ index.js
@@ -16,38 +16,8 @@ const perde = {
 
 // aqui vamos inserir a lógica do jogo
 app.post('/message', (req, res) => {  
-  console.log('nova mensagem', req.body.Body);
-  const usuario = req.body.Body.toLowerCase();
-  switch(usuario) {
-      case 'pedra':
-      case 'papel': 
-      case 'tesoura':
-          // fazer a escolha do computador e responder quem ganhou
-          const computador = opcoes[Math.floor(Math.random() * opcoes.length)];
-
-
-          if (computador === usuario) {
-              res.send('<Response><Message>Ops, deu empate!</Message></Response>')
-          } else {
-              if (perde[computador] === usuario) {
-                  // computador perdeu
-                  res.send(`<Response><Message>Eu escolhi *${computador}*</Message><Message>Você ganhou, mas quero jogar novamente!</Message></Response>`);
-              } else {
-                  // computador ganhou
-                  const twiml = new twilio.twiml.MessagingResponse();
-                  twiml.message(`Eu escolhi *${computador}*`);
-                  twiml.message('Ganhei! Ganhei!!!')
-                      .media('https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg');
-                  res.send(twiml.toString());
-              }
-          }
-          break;
-
-      default:
-          // tratar "fallback intent"
-          res.send('<Response><Message>Escolha Pedra, Papel ou Tesoura!</Message></Response>')
-          break;
-  }
+  console.log('nova mensagem', 'teste');
+  
   
 });
 

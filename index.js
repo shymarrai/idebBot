a3e47327a84d0460c8b0bd71e5b7fdd179b7d3e0
Bruno Gomes <icestonebruno@gmail.com>
2021-10-24 11:28:56 -0300

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
+require('dotenv').config()
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
